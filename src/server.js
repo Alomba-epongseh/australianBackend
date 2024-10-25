2ace3a78403a460cce0f16fd87e897b87cbadb92
Alomba-epongseh <alombangwingche@gmail.com>
2024-10-24 23:47:06 -0700

    changes done

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // import dotenv from 'dotenv';
 const dotenv = require('dotenv');
-const {ErrorHandler} = require('./middleware/index.mjs');
+import {ErrorHandler} from './middleware/index.mjs';
 const {apiRoute} = require('./routes/index.js');
 const cors = require('cors');
 const morgan = require('morgan');
